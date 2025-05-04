2f76f45d395afd676b4afbf41d5453b60d78c66e
shuk29 <shuklaghosh775@gmail.com>
2025-05-05 02:01:22 +0600

    testing a sample

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("LMAO")
+console.log("This is a single line.");
+
+let name = "John";
+let age = 30;
+console.log("Name:", name, "Age:", age);
+
+console.log("Name: " + name + ", Age: " + age);
+
+console.log(`Name: ${name}, Age: ${age}`);
